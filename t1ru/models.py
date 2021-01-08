7fadc30a9b5e369a7b0b886409b907113e76efeb
keyboard2543 <keyboard2543@gmail.com>
2021-01-08 18:32:17 +0700

    Implement models

--- t1ru/models.py
+++ t1ru/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
 class HomeCarousel(models.Model):
-    pass
+    label = models.CharField(blank=True)
+    description = models.CharField(blank=True)
+
+
+class Announcement(models.Model):
+    topic = models.CharField(blank=False)
+    description = models.TextField(blank=False)
+    pub_date = models.DateTimeField(auto_now_add=True, editable=False)
+    image = models.ImageField(blank=True, upload_to="carousel-image")
+
+
+class Personnel(models.Model):
+    name_title = models.CharField(blank=False)
+    name = models.CharField(blank=False)
+    lase_name = models.CharField(blank=False)
+    photo = models.ImageField(blank=True)
