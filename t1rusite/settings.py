c8109d716b0f982ee18c7cf7b47378f1ee4242f0
keyboard2543 <keyboard2543@gmail.com>
2021-12-25 17:19:03 +0700

    😘 Python Decouple

--- t1rusite/settings.py
+++ t1rusite/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '22-fnc^)5@#9f6lo_w_00$^nei^o@xq5mqhv+#ta60#!qw7kwc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['t1ru.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
